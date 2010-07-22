f4f234b77ac78c6b2d66e82f1fc678552854d830
beth <beth@qut.edu.au>
2010-07-22 11:21:20 +1000

    added data and matlab

---
+++ matlab/readit1.m
@@ -0,0 +1,18 @@
+av = zeros(6,50);
+stddev = zeros(6,50);
+fid = fopen('testTypeProliferation.txt','r');
+for (i=1:6),
+hdr = fscanf(fid,'%s ',3);
+vals = fscanf(fid,'%f%f%f',[3 50]);
+av(i,:) = vals(2,:);
+stddev(i,:) = vals(3,:);
+end
+frac = vals(1,:);
+figure(1);clf;
+errorbar(frac,av(2,:),stddev(2,:),'kx');
+hold on;
+errorbar(frac,av(3,:),stddev(3,:),'bo');
+hold on;
+errorbar(frac,av(4,:),stddev(4,:),'r*');
+hold on;
+errorbar(frac,av(5,:),stddev(5,:),'gv');
